450ab6e95f74acf4ccc8ee8ea55cad7cd1da2854
Joaquin-mlg <joaquin2012guillen@gmail.com>
2025-11-10 17:36:24 -0500

    primeraparteminijuego
    skech, reemplazar las imagenes por las finales

--- game/scripts/historia_1.gd
+++ game/scripts/historia_1.gd
@@ -2,9 +2,10 @@ extends Node2D
 
 @onready var sprite = $Sprite2D
 var imagenes = [
-	#preload("res://game/assets/fondos/historia1_1.png"),
-	#preload("res://game/assets/fondos/historia1_2.png"),
-	#preload("res://game/assets/fondos/historia1_3.png")
+	preload("res://game/assets/fondos/historia1_1.png"),
+	preload("res://game/assets/fondos/historia1_2.png"),
+	preload("res://game/assets/fondos/historia1_3.png"),
+	preload("res://game/assets/fondos/historia1_4.png")
 ]
 var indice = 0
 
@@ -13,8 +14,12 @@ func _ready():
 	#$AudioStreamPlayer.play() # si quieres reproducir narración
 
 func _unhandled_input(event):
-	if event is InputEventScreenTouch and event.pressed:
-		_on_next_pressed()
+	if (event is InputEventScreenTouch and event.pressed) or (event is InputEventMouseButton and event.pressed):
+		indice += 1
+		if indice < imagenes.size():
+			sprite.texture = imagenes[indice]
+		else:
+			get_tree().change_scene_to_file("res://game/minijuegos/esquivar/Esquivar.tscn")
 
 func _on_next_pressed():
 	indice += 1
--- game/scripts/main_menu.gd
+++ game/scripts/main_menu.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
 
-func _ready():
-	$CanvasLayer/Control/Button.pressed.connect(_on_start_pressed)
+#func _ready():
+#	$CanvasLayer/Control/Button.pressed.connect(_on_start_pressed)
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://game/historia/Historia1.tscn")
 
 
 func _on_button_iniciar_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://game/historia/historia1.tscn")
